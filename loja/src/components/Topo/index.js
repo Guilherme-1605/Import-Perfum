7aae8688adca63ed977ecbe149615b0c9c974228
Guilherme-1605 <guilhermepenna155@gmail.com>
2025-08-07 17:35:45 -0300

    Iniciando Projeto

---
+++ loja/src/components/Topo/index.js
@@ -0,0 +1,17 @@
+export default function Topo() {
+    return (
+        <header>
+            <div>
+                <picture>
+                    <a href="/"><img src="./logo.png"></img> </a>
+                </picture>
+
+                <nav>
+                    <a herf="#">Produtos</a>
+                    <a herf="#">Sobre</a>
+                    <a herf="#">Contato</a>
+                </nav>
+            </div>
+        </header>
+    )
+}
